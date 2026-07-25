f4281a3035c3ac415d5fdb2ba164490fa2b5a228
Mohamed Khaled El-shayp <mohamedkhaledmahmoud97@gmail.com>
2026-07-25 19:52:03 +0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+"""
+ALSHAYEB LAW
+
+Egyptian Legal Retrieval-Augmented Generation (RAG) System
+for Legal Research and Academic Benchmarking.
+"""
+
+__version__ = "0.1.0"
+
+__author__ = "Mohamed Khaled Mahmoud Ibrahim "ALSHAYEB""
+
+__project__ = "ALSHAYEB LAW"
